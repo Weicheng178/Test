ee1abd4fd8283bc644f2ed0a236d7edfe7d1587e
Weicheng <weichengli4711@gmail.com>
2022-11-15 23:35:05 +0100

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(1);
+            fonction();
+        }
+
+        public static void fonction()
+        {
+            //wait user to enter a caracter line
+            Console.WriteLine("please enter a caracter line: ");
+            //user enter what he wants
+            string line = Console.ReadLine();
+            Console.WriteLine("what you entered is: " + line);
+            //upcase these caracters
+            line = line.ToUpper();
+            Console.WriteLine("the result is: " + line);
+        }
+    }
+}
