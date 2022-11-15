ffdb37c433273ad7770ba1d4b3a4bdb46c2c1623
Weicheng <weichengli4711@gmail.com>
2022-11-15 23:39:16 +0100

    第一次修改

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1
         public static void Main(string[] args)
         {
             Console.WriteLine(1);
+            Console.WriteLine(1);
+            Console.WriteLine(1);
             fonction();
         }
 
