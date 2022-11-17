e934daf32b1c6886926a21074eb44c403def87f3
Weicheng <118312884+Weicheng178@users.noreply.github.com>
2022-11-17 23:06:15 +0100

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-        
+            Console.WriteLine(1);
             Console.WriteLine(1);
             fonction();
         }
