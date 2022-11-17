97989e5257e70300aa2279c6df3b85a2cb3e4852
Weicheng <weichengli4711@gmail.com>
2022-11-18 00:27:26 +0100

    11

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,6 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-            
-            Console.WriteLine(2);
-            Console.WriteLine(1);
-            fonction();
-            fonction();
             fonction();
         }
 
