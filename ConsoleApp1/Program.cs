168d7a50a33d7b376a987f984fb4d65761bc749d
Weicheng <weichengli4711@gmail.com>
2022-11-20 13:30:31 +0100

    111

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,7 +16,8 @@ namespace ConsoleApp1
             Vuemodel vuemodel = new Vuemodel();
             vuemodel.SaisieText();
             vuemodel.CheckText();
-            
+            vuemodel.CheckText();
+
             string line = Console.ReadLine();
 
         }
